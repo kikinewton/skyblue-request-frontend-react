53e45342e043d72413c576197c27eb2946742a84
ernest etornam anyidoho <etornamanyidoho@outlook.com>
2021-12-14 12:14:13 +0000

    Updates

---
+++ static/js/30.7030856c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[30],{1164:function(t,e,r){"use strict";r.r(e);var n=r(527),s=r(300),a=(r(1),r(22)),c=r(44),o=r(13);e.default=function(){var t=Object(a.g)();return Object(o.jsx)(n.a,{status:"403",title:"403",subTitle:"Sorry, you are not authorized to access this page.",extra:Object(o.jsx)(s.a,{type:"primary",onClick:function(){return t.push(c.d)},children:"Back Home"})})}}}]);
+//# sourceMappingURL=30.7030856c.chunk.js.map
--- static/js/30.ca560464.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[30],{1167:function(t,e,r){"use strict";r.r(e);var n=r(529),s=r(302),a=(r(1),r(20)),c=r(44),o=r(13);e.default=function(){var t=Object(a.g)();return Object(o.jsx)(n.a,{status:"403",title:"403",subTitle:"Sorry, you are not authorized to access this page.",extra:Object(o.jsx)(s.a,{type:"primary",onClick:function(){return t.push(c.d)},children:"Back Home"})})}}}]);
-//# sourceMappingURL=30.ca560464.chunk.js.map
