953a0cf576d637d82d21c7e48882561123268677
ernest etornam anyidoho <etornamanyidoho@outlook.com>
2021-12-16 22:01:26 +0000

    approve pettycash

---
+++ src/containers/MyRequest/components/MyRequestMenu.js
@@ -0,0 +1,103 @@
+import { PlusCircleOutlined } from '@ant-design/icons'
+import { Menu } from 'antd'
+import React, { useState, useEffect } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
+
+
+const MyRequestMenu = props => {
+  const [current, setCurrent] = useState("my-lpos")
+  const location = useLocation()
+  const history = useHistory()
+
+  useEffect(() => {
+    const { pathname } = location
+    let key = "my-lpos"
+    if(pathname.includes("/lpos/add-new")) {
+      key= "create-request"
+    } else if(pathname.includes("/petty-cash-requests/add-new")) {
+      key= "create-petty-cash"
+    } else if(pathname.includes("/float-requests/add-new")) {
+      key= "create-float"
+    } else if(pathname.includes("/my-requests/lpos")) {
+      key = "my-lpos"
+    } else if(pathname.includes("/my-requests/petty-cash-requests")) {
+      key = "my-petty-cash-requests"
+    } else if(pathname.includes("/my-requests/float-requests")) {
+      key = "my-float-requests"
+    } else {
+      key = "create-request"
+    }
+    setCurrent(key)
+  }, [current])
+
+  return (
+    <>
+      <Menu
+        mode="horizontal"
+        selectedKeys={[current]}
+      >
+        <Menu.Item 
+          key="my-lpos"
+          onClick={()=> {
+            setCurrent("my-lpos")
+            history.push("/app/my-requests/lpos")
+          }}
+        >
+          LPO Requests
+        </Menu.Item>
+        <Menu.Item 
+          key="my-petty-cash-requests"
+          onClick={() => {
+            setCurrent("my-petty-cash-requests")
+            history.push("/app/my-requests/petty-cash-requests")
+          }}
+        >
+          Petty Cash Requests
+        </Menu.Item>
+        <Menu.Item 
+          key="my-float-requests"
+          onClick={() => {
+            setCurrent("my-float-requests")
+            history.push("/app/my-requests/float-requests")
+          }}
+        >
+          Float Requests
+        </Menu.Item>
+        <Menu.SubMenu key="create-request-submenu" title="Create Request">
+          <Menu.Item
+            key="create-request"
+            onClick={() => {
+              setCurrent("create-request")
+              history.push("/app/my-requests/lpos/add-new")
+            }}
+          >
+            <PlusCircleOutlined />
+            New LPO Request
+          </Menu.Item>
+          <Menu.Item
+            key="create-petty-cash"
+            onClick={() => {
+              setCurrent("create-petty-cash")
+              history.push("/app/my-requests/petty-cash-requests/add-new")
+            }}
+          >
+            <PlusCircleOutlined />
+            New Petty Cash Request
+          </Menu.Item>
+          <Menu.Item
+            key="create-float"
+            onClick={() => {
+              setCurrent("create-float")
+              history.push("/app/my-requests/float-requests/add-new")
+            }}
+          >
+            <PlusCircleOutlined />
+            New Float Request
+          </Menu.Item>
+        </Menu.SubMenu>
+      </Menu>
+    </>
+  )
+}
+
+export default MyRequestMenu
