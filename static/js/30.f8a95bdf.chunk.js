6cedf8ad80438d483faf53ce96a3e91988bd50b2
ernest etornam anyidoho <etornamanyidoho@outlook.com>
2021-12-23 16:09:08 +0000

    Updates

---
+++ static/js/30.f8a95bdf.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[30],{1436:function(t,e,r){"use strict";r.r(e);var n=r(609),s=r(307),a=(r(1),r(25)),c=r(54),o=r(14);e.default=function(){var t=Object(a.g)();return Object(o.jsx)(n.a,{status:"403",title:"403",subTitle:"Sorry, you are not authorized to access this page.",extra:Object(o.jsx)(s.a,{type:"primary",onClick:function(){return t.push(c.d)},children:"Back Home"})})}}}]);
+//# sourceMappingURL=30.f8a95bdf.chunk.js.map
