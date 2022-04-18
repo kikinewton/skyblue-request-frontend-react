1f4eabdcb22d5220d485b20bbec8441910d5bc16
ernest etornam anyidoho <etornamanyidoho@outlook.com>
2022-04-18 17:30:42 +0000

    deploy

---
+++ src/shared/GoodsReceivedNoteDetails.js
@@ -0,0 +1,119 @@
+import React, { useState } from 'react'
+import { Row, Col, List, Image, Card, Table, Button } from "antd"
+import { BASE_URL } from '../services/api/urls'
+import { formatCurrency, prettifyDateTime } from '../util/common-helper'
+import { DownloadOutlined } from '@ant-design/icons'
+import { generateResourceUrl } from '../services/api/document'
+import MyPdfView from '../presentation/MyPdfView'
+import Proptypes from "prop-types"
+import FilesView from './FilesView'
+
+const columns = [
+  {
+    title: "Item Description",
+    dataIndex: "name",
+    key: "name"
+  },
+  {
+    title: "Unit Price",
+    dataIndex: "unitPrice",
+    key: "unitPrice"
+  },
+  {
+    title: "Quantity",
+    dataIndex: "quantity",
+    key: "quantity"
+  },
+
+]
+
+
+const GoodsReceivedNoteDetails = (props) => {
+  const {
+    grn,
+    invoice,
+    invoiceDocument,
+    requestItems
+  } = props
+  const [imagePreview, setImagePreview] = useState(false)
+  return (
+    <>
+      <Card title="Goods Received Note" style={{marginBottom: 10}} size="small">
+        <Row>
+          <Col span={12}>
+            <List 
+              itemLayout="horizontal"
+            >
+              <List.Item key="invoiceNumber">
+                <List.Item.Meta title="Invoice Number" description={invoice?.invoiceNumber} />
+              </List.Item>
+              <List.Item key="createdOn">
+                <List.Item.Meta title="Created On" description={prettifyDateTime(grn?.createdDate)} />
+              </List.Item>
+              <List.Item>
+                <List.Item.Meta title="Invoice Created By" description={grn?.createdBy?.fullName} />
+              </List.Item>
+            </List>
+          </Col>
+          <Col span={12}>
+            <List>
+              <List.Item key="supplier">
+                <List.Item.Meta title="Supplier" description={invoice?.supplier?.name} />
+              </List.Item>
+              <List.Item key="amount">
+                <List.Item.Meta title="Amount" description={formatCurrency(grn?.invoiceAmountPayable)} />
+              </List.Item>
+              <List.Item>
+                <List.Item.Meta title="Local Purchase Order Reference" description={grn?.localPurchaseOrder?.lpoRef} />
+              </List.Item>
+            </List>
+          </Col>
+        </Row>
+        <Row style={{marginTop: 20}}>
+          <Col><span style={{fontWeight: "bold"}}>Supporting Docuemnt(Goods Received Note)</span></Col>
+        </Row>
+        <Row style={{minHeight: 200, border: "#000 1px solid"}}>
+          <Col span={24}>
+            <Row>
+              <Col span={24} style={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "100%"}}>
+                <FilesView files={[invoiceDocument]} />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+          {requestItems && (
+            <>
+              <Row style={{marginTop: 20}}>
+                <Col><span style={{fontWeight: "bold"}}>Received Items</span></Col>
+              </Row>
+              <Row>
+                <Col span={24}>
+                  <Table 
+                    columns={columns}
+                    dataSource={grn?.receivedItems}
+                    bordered
+                    size="small"
+                    pagination={false}
+                    rowKey="id"
+                  />
+                </Col>
+              </Row>
+            </>
+          )}
+          </Col>
+        </Row>
+      </Card>
+    </>
+  )
+}
+
+GoodsReceivedNoteDetails.propTypes = {
+  grn: Proptypes.object,
+  invoice: Proptypes.object,
+  invoiceDocument: Proptypes.object,
+  requestItems: Proptypes.array
+}
+
+export default GoodsReceivedNoteDetails
---
+++ src/shared/PaymentDetails.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import PropTypes from "prop-types"
+import { Card, List, Row, Col } from 'antd'
+import { formatCurrency, prettifyDateTime } from '../util/common-helper'
+
+const PaymentDetails = (props) => {
+  const {
+    payment
+  } = props
+
+  return (
+    <Card size='small' title="Payment Details">
+      <Row>
+        <Col span={11}>
+          <List>
+            <List.Item>
+              <List.Item.Meta title="Purchase Number" description={payment?.purchaseNumber} />
+            </List.Item>
+            <List.Item>
+              <List.Item.Meta title="Payment Method" description={payment?.paymentMethod} />
+            </List.Item>
+            <List.Item>
+              <List.Item.Meta title="Payment Status" description={payment?.paymentStatus} />
+            </List.Item>
+            <List.Item>
+              <List.Item.Meta title="Payment By" description={`${payment?.createdBy?.fullName} (${payment?.createdBy?.email})`} />
+            </List.Item>
+            <List.Item>
+              <List.Item.Meta title="Payment Method" description={payment?.paymentMethod} />
+            </List.Item>
+          </List>
+        </Col>
+        <Col span={11} offset={2}>
+          <List>
+            <List.Item>
+              <List.Item.Meta title="Cheque Number" description={payment?.chequeNumber} />
+            </List.Item>
+            <List.Item>
+              <List.Item.Meta title="Payment Date" description={prettifyDateTime(payment?.createdDate)} />
+            </List.Item>
+            <List.Item>
+              <List.Item.Meta title="Payment Withholding Tax(%)" description={payment?.withholdingTaxPercentage} />
+            </List.Item>
+            <List.Item>
+              <List.Item.Meta title="Amount Paid" description={formatCurrency(payment?.paymentAmount)} />
+            </List.Item>
+            <List.Item>
+              <List.Item.Meta title="WithHolding Amount" description={formatCurrency(payment?.withholdingTaxAmount)} />
+            </List.Item>
+          </List>
+        </Col>
+      </Row>
+    </Card>
+  )
+}
+
+PaymentDetails.propTypes = {
+  payment: PropTypes.object
+}
+
+export default PaymentDetails
